8cd90bfcf56057d865549503225c333ac4816422
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-07-07 13:52:31 +0800

    feat: add Prisma setup with initial database schema and seeding script
    - Updated package.json to include new scripts for database operations and Docker services.
    - Created prisma.config.ts for Prisma configuration.
    - Added initial migration SQL for User, Account, and Role tables.
    - Created migration lock file for PostgreSQL provider.
    - Defined Prisma schema with models for User, Account, and Role.
    - Implemented seed script to populate default roles in the database.
    - Removed unused AppController and AppService files to clean up the project structure.
    - Integrated ConfigModule for environment variable management.

---
+++ prisma/migrations/20260707053526_init/migration.sql
@@ -0,0 +1,61 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "image" TEXT,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "roleId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "verificationToken" TEXT,
+    "verificationTokenExpiresAt" TIMESTAMP(3),
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" TIMESTAMP(3),
+    "refreshTokenHash" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Account_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Role" (
+    "id" TEXT NOT NULL,
+    "code" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_userId_key" ON "Account"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_verificationToken_key" ON "Account"("verificationToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_resetToken_key" ON "Account"("resetToken");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Role_code_key" ON "Role"("code");
+
+-- AddForeignKey
+ALTER TABLE "User" ADD CONSTRAINT "User_roleId_fkey" FOREIGN KEY ("roleId") REFERENCES "Role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
