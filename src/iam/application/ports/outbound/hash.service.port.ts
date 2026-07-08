406ded66f838fb54f749387e2211a0084ae3527a
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-07-08 23:27:13 +0800

    feat: implement login functionality with command and use case; add login DTO and update auth controller

--- src/iam/application/ports/outbound/hash.service.port.ts
+++ src/iam/application/ports/outbound/hash.service.port.ts
@@ -1,3 +1,5 @@
 export abstract class HashServicePort {
 	abstract hash(plainText: string, length: number): Promise<string>;
+
+	abstract compare(currentValue: string, existingValue: string): Promise<boolean>;
 }
---
+++ src/iam/application/use-cases/login-user.use-case.ts
@@ -0,0 +1,60 @@
+import { Injectable } from "@nestjs/common";
+import {
+	LoginUserCommand,
+	LoginUserResult,
+	LoginUserUseCasePort,
+} from "../ports/inbound/login-user.in-port";
+import { IamRepositoryPort } from "../ports/outbound/iam.repository.port";
+import { JwtServicePort } from "../ports/outbound/jwt.service.port";
+import { HashServicePort } from "../ports/outbound/hash.service.port";
+import { Email } from "@/iam/domain/value-objects/email.value-object";
+import { InvalidLoginException } from "../exceptions/application.exception";
+
+@Injectable()
+export class LoginUserUseCase implements LoginUserUseCasePort {
+	constructor(
+		private readonly iamRepository: IamRepositoryPort,
+		private readonly jwtService: JwtServicePort,
+		private readonly hashService: HashServicePort,
+	) {}
+
+	public async execute(command: LoginUserCommand): Promise<LoginUserResult> {
+		const emailVo = new Email(command.email);
+
+		const user = await this.iamRepository.findByEmail(emailVo);
+
+		if (!user || !user.account) throw new InvalidLoginException("Invalid email or password.");
+
+		const passwordMatch = await this.hashService.compare(
+			command.password,
+			user.account?.getPasswordHash(),
+		);
+
+		if (!passwordMatch) throw new InvalidLoginException("Invalid email or password.");
+
+		if (!user.isVerified)
+			throw new InvalidLoginException("Please verify your email address before logging in.");
+
+		const tokens = await this.jwtService.generateTokens({
+			sub: user.id,
+			email: user.email.getValue(),
+			role: user.role.code,
+		});
+
+		const refreshTokenHash = await this.hashService.hash(tokens.refreshToken, 10);
+		user.account.updateRefreshTokenHash(refreshTokenHash);
+
+		await this.iamRepository.save(user);
+
+		return {
+			accessToken: tokens.accessToken,
+			refreshToken: tokens.refreshToken,
+			user: {
+				id: user.id,
+				email: user.email.getValue(),
+				name: user.name,
+				role: user.role.code,
+			},
+		};
+	}
+}
