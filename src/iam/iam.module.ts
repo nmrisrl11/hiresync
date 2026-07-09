83e06f4f17d26dcd0f345b3a20a18dbfcb43618a
Nomer Icban Israel <46095783+nmrisrl11@users.noreply.github.com>
2026-07-09 10:46:34 +0800

    feat: implement login use case with command and result types; update auth controller and module imports

--- src/iam/iam.module.ts
+++ src/iam/iam.module.ts
@@ -1,7 +1,7 @@
 import { DatabaseModule } from "@/shared/database/database.module";
 import { QueueModule } from "@/shared/queue/queue.module";
 import { Module } from "@nestjs/common";
-import { LoginUserUseCasePort } from "./application/ports/inbound/login-user.in-port";
+import { LoginUseCasePort } from "./application/ports/inbound/login.in-port";
 import { LogoutUseCasePort } from "./application/ports/inbound/logout.in-port";
 import { RegisterUserUseCasePort } from "./application/ports/inbound/register-user.in-port";
 import { VerifyEmailUseCasePort } from "./application/ports/inbound/verify-email.in-port";
@@ -11,7 +11,7 @@ import { IamRepositoryPort } from "./application/ports/outbound/iam.repository.p
 import { IdGeneratorPort } from "./application/ports/outbound/id-generator.port";
 import { JwtServicePort } from "./application/ports/outbound/jwt.service.port";
 import { VerificationTokenGeneratorPort } from "./application/ports/outbound/verification-token-generator.port";
-import { LoginUserUseCase } from "./application/use-cases/login-user.use-case";
+import { LoginUseCase } from "./application/use-cases/login.use-case";
 import { LogoutUseCase } from "./application/use-cases/logout.use-case";
 import { RegisterUserUseCase } from "./application/use-cases/register-user.use-case";
 import { VerifyEmailUseCase } from "./application/use-cases/verify-email.use-case";
@@ -28,7 +28,7 @@ import { AuthController } from "./presentation/controllers/auth.controller";
 	providers: [
 		{ provide: RegisterUserUseCasePort, useClass: RegisterUserUseCase },
 		{ provide: VerifyEmailUseCasePort, useClass: VerifyEmailUseCase },
-		{ provide: LoginUserUseCasePort, useClass: LoginUserUseCase },
+		{ provide: LoginUseCasePort, useClass: LoginUseCase },
 		{ provide: LogoutUseCasePort, useClass: LogoutUseCase },
 
 		{ provide: IamRepositoryPort, useClass: PrismaIamRepository },
